a95f01899a5eb847c67eee4fee54e94444dc121a
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-02 18:23:21 +0300

    2.9.2022 V.1

---
+++ src/GitGithubNedir.java
@@ -0,0 +1,44 @@
+public class GitGithubNedir {
+
+    /*
+    Git-Github versiyon kontrol sistemidir(VKS) -> version control system
+
+    3 tip versiyon sistemi vardir
+    1)Yerel (Local) vks => kendi bilgisayarimizda calistigimiz
+    2)Merkezi (Central) vks => localde calistigimiz dosya-klasorleri cloud a gondeririz
+    3)Daginik (Distributed) vks => en yeni versiyon kontrol sistemidir. Projeyi cloud(uzaktan) dab
+                                   kendi locallimize cekip calistigimiz vks dir.
+
+  */
+
+    /*
+              Git-Github Nedir? Farkliliklari Nelerdir?
+
+              GIT
+                                                               GITHUB
+        -> Git dedigimizde kendi bilgisayarimiz            ->Github dedigimizde aklimiza remote, cloud
+        aklimiza gelebilir. Calismalarimizi kendi           gelebilir. Projelerimizi uzak repoya gonderdigimiz
+        local imizda yaptigimiz yerdir.                     yerdir.
+       -> Tek basina calisma                               ->Calismalarimiz Github da yedeklenebilir (Backup)
+       ->Hataları geri alabilmek                           ->Projelerimizi yayinlayabilir
+                                                           ->Projelerimizi takim arkadaslarimizla paylasabiliriz (Share)
+                                                           ->Takim arkadaslarimizla ortak calisma yurutebiliriz (Collobration)
+
+
+       -->Git ile Github'in farki: Git'de projeyi kendi local (bilgisayarimizda) calisiriz.
+       -->Github'da projelerimizi uzakta depoladigimiz (respo) yerdir.
+       -->Git'de projede kendimize ait kisimi gelistirirken; Githubda paylastigimiz projeleri birlestirip bir grup halinde
+       calisiriz.
+       -->Github sayesinde kendi survel imizin basina bir sey gelse bile baska bir survel kurup kodlarimizi oarya aktarabiliriz.
+
+
+*/
+
+
+
+
+
+
+
+
+}
