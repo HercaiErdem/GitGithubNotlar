f3bc8220710eb443346a000ea805f9120f998b17
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-05 03:10:58 +0300

    5.9.2022

---
+++ src/GithubinEnYayginKomutlari.java
@@ -0,0 +1,73 @@
+public class GithubinEnYayginKomutlari {
+
+    // Komutlar, IntelliJ'de Terminal de yazilacak
+
+
+    /*
+    git init --> Her yeni proje ollusturdugumuzda sadece bir kereligine bu komut kullanilir.
+                  Local repomuz(Git) ile cloud daki(Github) iliskilendirilir
+                  (Initialize a local Git repository)
+    git status --> Working Space ve Staging Area daki durumu görmek icin (Check status)
+    git add . --> Working Area da yazdigimiz code lari bu komut ile Staging Area ya gonderiyoruz
+                  (Send a codes to the staging area)
+    git commit -m "...." --> Commit Store a gönderiyoruz
+    git log --oneline --> Commit Store a giden codelarin durumunu görmek icin kullanilir
+                          (Check codes status on Commit Store, briefly)
+    git branch --> Mevcut branch leri listeler (Current list branches)
+    git branch [branch name] --> Yeni branch olusturmak icic , köseli pzantez OLMADAN
+                                 (Create a new branch)
+   git branch -d [branch name] --> Silmek istedigimiz branch in ismini yazip siliyoruz
+                                   (Delete a branch that we want)
+   git branch -m [new branch name] --> Localdeki branch in adini degistirir
+                                                         (Rename a local branch)
+   git checkout [branch name] --> Istedigimiz branch e geceriz (Switch to a branch)
+   git merge [branch name] --> 2 branch i birlestirir, esitler, update eder
+
+   git remote add origin [Githubdaki adress] --> Localimizdeki codelari cloud a gondermek icin kullanilir
+                                                 Bu kod, her actigimiz yeni projelerde ilk defa push ediyorsak
+                                                  sadece1 defaligina kullanilir
+                                            (Add a remote repository, use just once time for every new project we create)
+git push -u origin master/main --> Bu komut da sadece1 defaligina kullanilir
+git push
+git pull --> Remote dan localle aldigimiz en uncel kodlar (Update local repository to the newest commit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+      */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
